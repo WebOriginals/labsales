99c13cd3de51c868fb94f688b7c9212f9b959d45
WebOriginals <web-originals@yandex.ru>
2021-04-22 18:58:27 +0300

    пооборачивал скрипты

--- labsales/js/app.js
+++ labsales/js/app.js
@@ -669,56 +669,60 @@ var galleryTop = new Swiper('.main-container', {
         clickable: true,
     },
 });
+    if ($('.project-container').length > 0) {
     var project = new Swiper('.project-container', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    loop:true,
-    centeredSlides: true,
-    navigation: {
-        nextEl: '.project-button-next',
-        prevEl: '.project-button-prev',
-    },
-    pagination: {
-        el: '.project-pagination',
-        dynamicBullets: true,
-        clickable: true,
-    },
-    breakpoints: {
-        // when window width is >= 320px
-        320: {
-            slidesPerView: 1,
-            spaceBetween: 20
+        slidesPerView: 3,
+        spaceBetween: 30,
+        loop: true,
+        centeredSlides: true,
+        navigation: {
+            nextEl: '.project-button-next',
+            prevEl: '.project-button-prev',
         },
+        pagination: {
+            el: '.project-pagination',
+            dynamicBullets: true,
+            clickable: true,
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
 
-        767: {
-            slidesPerView: 1.5,
-            spaceBetween: 30
-        },
-        1200: {
-            slidesPerView: 2,
-            spaceBetween: 160
-        },
-        1600: {
-            slidesPerView: 2.5,
-            spaceBetween: 180
-        },
-        2500: {
-            slidesPerView: 3,
-            spaceBetween: 250
+            767: {
+                slidesPerView: 1.5,
+                spaceBetween: 30
+            },
+            1200: {
+                slidesPerView: 2,
+                spaceBetween: 160
+            },
+            1600: {
+                slidesPerView: 2.5,
+                spaceBetween: 180
+            },
+            2500: {
+                slidesPerView: 3,
+                spaceBetween: 250
+            }
         }
-    }
-});
+    });
+}
+    if ($('.reviews-container').length > 0) {
     var reviews = new Swiper('.reviews-container', {
-    autoHeight: true,
-    spaceBetween: 30,
-    loop:true,
-    centeredSlides: true,
-    navigation: {
-        nextEl: '.reviews-button-next',
-        prevEl: '.reviews-button-prev',
-    },
+        autoHeight: true,
+        spaceBetween: 30,
+        loop: true,
+        centeredSlides: true,
+        navigation: {
+            nextEl: '.reviews-button-next',
+            prevEl: '.reviews-button-prev',
+        },
 
-});
+    });
+}
     // end sliders
 
 

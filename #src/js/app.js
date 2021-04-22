342f46b417b5e85ab5c404d7054f03c30444136d
WebOriginals <web-originals@yandex.ru>
2021-04-22 19:00:57 +0300

    убрал из проекта все лишнее

--- #src/js/app.js
+++ #src/js/app.js
@@ -2,11 +2,6 @@
 $( document ).ready(function() {
     //component
     @@include('../component/Modals/js/modal.js')
-    @@include('../component/Tabs/js/tabs.js')
-    @@include('../component/InputList/js/InputList.js')
-    @@include('../component/dropDownLlist/js/dropDownList.js')
-    @@include('../component/Quantity/js/quantity.js')
-    @@include('../component/map/js/map.js')
     @@include('../component/dinamicAdaptiv/js/dinamicAdaptiv.js')
     @@include('../component/lazyLoad/js/lazyLoad.js')
     // end component
